3ba8a0fc55e169b456b87aee457babe50325b62f
Santiago <santy4lt@gmail.com>
2025-06-29 16:31:33 -0300

    Ejercicio 2 en rama CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
 Console.WriteLine("Hello, World!");
 int a;
 int b;
@@ -38,5 +40,115 @@ if (numeroI > 0)
 }
 else
 {
-    Console.WriteLine(numeroI);
+    Console.WriteLine();
+}
+
+
+
+//TP6 - EJERCICIO 2 - Altamirano Santiago Joaquin
+string opcionS;
+int opcionI;
+bool opcionB;
+float operando1=0, operando2=0;
+string operando1S, operando2S;
+bool operando1B, operando2B;
+
+Console.WriteLine("\nOpcion 0: Salir del programa");
+Console.WriteLine("Opcion 1: Sumar");
+Console.WriteLine("Opcion 2: Restar");
+Console.WriteLine("Opcion 3: Multiplicar");
+Console.WriteLine("Opcion 4: Dividir");
+do
+{
+    do
+    {
+        Console.WriteLine("Ingrese una opcion para continuar (0 para salir): ");
+        opcionS = Console.ReadLine();
+        opcionB = int.TryParse(opcionS, out opcionI);
+        if (!opcionB)
+        {
+            Console.WriteLine("opcion invalida");
+        }
+    } while (!opcionB || opcionI<0 ||opcionI>4);
+
+    if (opcionI != 0)
+    { 
+        Console.WriteLine("Ingrese 2 numeros para operar");
+        do
+        {
+            Console.WriteLine("primer operando: ");
+            operando1S = Console.ReadLine();
+            operando1B = float.TryParse(operando1S, out operando1);
+            if (!operando1B)
+            {
+                Console.WriteLine("numero invalido");
+            }
+        } while (!operando1B);
+
+        do
+        {
+            Console.WriteLine("segundo operando: ");
+            operando2S = Console.ReadLine();
+            operando2B = float.TryParse(operando2S, out operando2);
+            if (!operando2B)
+            {
+                Console.WriteLine("numero invalido");
+            }
+        } while (!operando1B);
+    }
+    switch (opcionI)
+    {
+        case 1:
+            Sumar(operando1, operando2);
+            break;
+        case 2:
+            Restar(operando1, operando2);
+            break;
+        case 3:
+            Multiplicar(operando1, operando2);
+            break;
+        case 4:
+            if (operando2 != 0)
+            {
+                Dividir(operando1, operando2);
+            }
+            else
+            {
+                Console.WriteLine("El segundo operando no puede ser 0!!!");
+            }
+            break;
+        default:
+            break;
+    }
+} while (opcionI!=0);
+
+
+
+
+void Sumar(float num1, float num2)
+{
+    float retorno;
+    retorno = num1 + num2;
+    Console.WriteLine($"La suma de {num1} + {num2} es {retorno}");
+}
+
+void Restar(float num1, float num2)
+{
+    float retorno;
+    retorno = num1 - num2;
+    Console.WriteLine($"La resta de {num1} - {num2} es {retorno}");
+}
+
+void Multiplicar(float num1, float num2)
+{
+    float retorno;
+    retorno = num1 * num2;
+    Console.WriteLine($"El producto de {num1} * {num2} es {retorno}");
+}
+
+void Dividir(float num1, float num2)
+{
+    float retorno;
+    retorno = num1 / num2;
+    Console.WriteLine($"La division de {num1} / {num2} es {retorno}");
 }
