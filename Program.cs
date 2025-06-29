bcc90184fda9c46cfee1ca90eabbc60c5628ea3c
Santiago <santy4lt@gmail.com>
2025-06-29 15:21:17 -0300

    cambios realizados en launch y tasks, ademas ejercicio 1 resuelto

--- Program.cs
+++ Program.cs
@@ -4,5 +4,39 @@ int a;
 int b;
 a=10;
 b=a;
-Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de a:" + a);
 Console.WriteLine("valor de b:" + b);
+
+
+
+
+
+
+
+
+//TP6 - EJERCICIO 1 - Altamirano Santiago Joaquin
+string numeroS;
+int numeroI;
+bool resultado;
+
+do
+{
+    Console.WriteLine("Ingrese un numero entero");
+    numeroS = Console.ReadLine();
+    resultado = int.TryParse(numeroS, out numeroI);
+} while (!resultado);
+
+//Console.WriteLine(resultado);
+
+if (numeroI > 0)
+{
+    while (numeroI != 0)
+    {
+        Console.Write(numeroI % 10);
+        numeroI = numeroI / 10;
+    }
+}
+else
+{
+    Console.WriteLine(numeroI);
+}
